7bcf3129d9e44e569c33786fcbddfaab02b9b244
Adi <adityasagarpython@gmail.com>
2025-02-26 14:13:35 -0400

    Feat: Working on start screen

---
+++ Assets/BlinkText.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+// blink "Press any key to start"
+public class BlinkText : MonoBehaviour
+{
+    private Image image;
+    [SerializeField] private float blinkTimeSpeed = 1.5f;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        SetOpacityWithRespectToTime();
+
+    }
+
+    private void SetOpacityWithRespectToTime()
+    {
+        var tempColor = image.color;
+        tempColor.a = Mathf.PingPong(Time.time * blinkTimeSpeed, 1);
+        image.color = tempColor;
+    }
+}
